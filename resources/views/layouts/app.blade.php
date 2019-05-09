96dcbf2c88f047f428c9fe276d298cfa0c1b339e
Nitin Kaware <nitin.kaware1@gmail.com>
2019-05-09 15:54:11 +0530

    svg svg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,17 +72,68 @@
                                     <span class="text-default">{{ auth()->user()->name }}</span>
                                         <small class="text-muted d-block mt-1">Administrator</small>
                                     </span>
-                                        
+
                                     </a>
                                     <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
                                         <a class="dropdown-item"
                                            href="#">
-                                            <i class="dropdown-icon fe fe-user"></i> Profile
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                 width="18"
+                                                 height="18"
+                                                 viewBox="0 0 24 24"
+                                                 fill="none"
+                                                 stroke="currentColor"
+                                                 stroke-width="2"
+                                                 stroke-linecap="round"
+                                                 stroke-linejoin="round"
+                                                 class="feather feather-user tw-inline"
+                                                 style="margin-left: -15px;">
+                                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                                <circle cx="12"
+                                                        cy="7"
+                                                        r="4"></circle>
+                                            </svg>
+                                            Profile
                                         </a>
 
                                         <router-link to="/my/wishlist"
                                                      class="dropdown-item">
-                                            <i class="dropdown-icon fe fe-user"></i>
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                 width="18"
+                                                 height="18"
+                                                 viewBox="0 0 24 24"
+                                                 fill="none"
+                                                 stroke="currentColor"
+                                                 stroke-width="2"
+                                                 stroke-linecap="round"
+                                                 stroke-linejoin="round"
+                                                 class="feather feather-list tw-inline"
+                                                 style="margin-left: -16px;">
+                                                <line x1="8"
+                                                      y1="6"
+                                                      x2="21"
+                                                      y2="6"></line>
+                                                <line x1="8"
+                                                      y1="12"
+                                                      x2="21"
+                                                      y2="12"></line>
+                                                <line x1="8"
+                                                      y1="18"
+                                                      x2="21"
+                                                      y2="18"></line>
+                                                <line x1="3"
+                                                      y1="6"
+                                                      x2="3"
+                                                      y2="6"></line>
+                                                <line x1="3"
+                                                      y1="12"
+                                                      x2="3"
+                                                      y2="12"></line>
+                                                <line x1="3"
+                                                      y1="18"
+                                                      x2="3"
+                                                      y2="18"></line>
+                                            </svg>
                                             My Wishlist
                                         </router-link>
 
@@ -91,7 +142,25 @@
                                            href="{{ route('logout') }}"
                                            onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
-                                            <i class="dropdown-icon fe fe-log-out"></i> @lang('Sign out')
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                 width="18"
+                                                 height="18"
+                                                 viewBox="0 0 24 24"
+                                                 fill="none"
+                                                 stroke="currentColor"
+                                                 stroke-width="2"
+                                                 stroke-linecap="round"
+                                                 stroke-linejoin="round"
+                                                 class="feather feather-log-out tw-inline"
+                                                 style="margin-left: -15px;">
+                                                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                                                <polyline points="16 17 21 12 16 7"></polyline>
+                                                <line x1="21"
+                                                      y1="12"
+                                                      x2="9"
+                                                      y2="12"></line>
+                                            </svg>
+                                            @lang('Sign out')
                                         </a>
                                         <form id="logout-form"
                                               action="{{ route('logout') }}"
